7b31c476f2489908bdc6dcadb3523b9328112910
zhoufei <zhoufei_zh@163.com>
2017-02-08 18:12:12 +0800

    add

--- rdfs-core/src/main/java/com/rdfs/core/contants/Constants.java
+++ rdfs-core/src/main/java/com/rdfs/core/contants/Constants.java
@@ -108,5 +108,7 @@ public class Constants {
 		public static final String DEALER_COMPANY_KEY = GLOBAL + "dealer_company_";
 		
 		public static final String USER_KEY = GLOBAL + "user_";
+		//菜单项
+		public static final String RESOURCE_KEY = GLOBAL + "resource_";
 	}
 }
