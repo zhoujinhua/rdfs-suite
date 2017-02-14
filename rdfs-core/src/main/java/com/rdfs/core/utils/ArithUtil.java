945a15c2bf0625cb71a5624a99b4f5e2bf8e0fcb
yanan.chen <yanan.chen@liyunqiche.com>
2017-02-14 16:28:10 +0800

    add tool

--- rdfs-core/src/main/java/com/rdfs/core/utils/ArithUtil.java
+++ rdfs-core/src/main/java/com/rdfs/core/utils/ArithUtil.java
@@ -10,7 +10,6 @@ public class   ArithUtil{
 	// 默认除法运算精度     
 	private static final int DEF_DIV_SCALE=10;
 	
-	
 	// 这个类不能实例化                                                                                                                            
 	private ArithUtil() {                                                                                                              
 	                                                                                                                             
